40e2cf7b632a71d562f1dee2b04ca0cbb0f03ddf
HalVil <142782950+HalVil@users.noreply.github.com>
2025-10-17 14:22:55 +0000

    Endret return

--- src/main/java/no/pgr301d/cddemo/CDController.java
+++ src/main/java/no/pgr301d/cddemo/CDController.java
@@ -8,7 +8,7 @@ class CDController {
 
     @GetMapping(path = "/")
     public String hello() {
-        return "Hello, this is a test";
+        return "Hello, this is a test number 2";
     }
 
 }
